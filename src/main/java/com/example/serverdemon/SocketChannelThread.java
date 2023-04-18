34bcf95d6784edb3429b3906def01099560d12eb
ziva456 <qkeksmsqkah@gmail.com>
2023-04-19 00:48:43 +0900

    Echo 기능

--- src/main/java/com/example/serverdemon/SocketChannelThread.java
+++ src/main/java/com/example/serverdemon/SocketChannelThread.java
@@ -9,14 +9,13 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Set;
 
 @Slf4j
 public class SocketChannelThread extends Thread{
     InetSocketAddress addr = new InetSocketAddress("localhost", 7777);
-
+    private final ByteBuffer buffer = ByteBuffer.allocate(1024);
     public void run() {
 
         try {
@@ -26,9 +25,7 @@ public class SocketChannelThread extends Thread{
             serverSocket.socket().bind(this.addr);				//서버주소 지정
             serverSocket.register(selector, SelectionKey.OP_ACCEPT);	//연결요청 수락모드로 설정
 
-
             while (!Thread.currentThread().isInterrupted()) {
-                ByteBuffer buffer = ByteBuffer.allocate(1024);
 
                 if(selector.select()>0){
                     Set<SelectionKey> selectedKeys = selector.selectedKeys();
@@ -37,28 +34,25 @@ public class SocketChannelThread extends Thread{
                         SelectionKey key = it.next();
 
                         if (key.isAcceptable()) {
-                            log.info("register");
+                            log.info("REGI");
                             register(selector, serverSocket);
                         }
-
-                        if (key.isConnectable()) {
-                            System.out.format("Connectable is for client.%n");
+                        //Echo
+                        if (key.isWritable()) {
+                            log.info("SEND");
+                            sendMessage(selector,key);
                         }
 
                         if (key.isReadable()) {
-                            log.info("readable");
-                            answerWithEcho(buffer,key);
+                            log.info("RECV");
+                            recvMessage(selector,key);
                         }
 
-                        if (key.isWritable()) {
-                            log.info("readable");
-                            System.out.format("Writable is activated.%n");
-                        }
                     it.remove();
                     }
                 }
-
             }
+            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -66,30 +60,43 @@ public class SocketChannelThread extends Thread{
         }
     }
 
-    private static void answerWithEcho(ByteBuffer buffer, SelectionKey key)
-            throws IOException, InterruptedException {
-        log.info("buffer : {}",buffer.remaining());
-        SocketChannel client = (SocketChannel) key.channel();
-        log.info("SocketChannel client : {}", client);
-        buffer.clear();
-
-        int bytesRead = client.read(buffer);
-        log.info("bytesRead : {}",bytesRead);
-
-        if (bytesRead == -1) {						//연결이 끊어졌을 때
-            throw new InterruptedException();
+    private void sendMessage(Selector selector, SelectionKey key) throws IOException, InterruptedException {
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        ByteBuffer buffer = (ByteBuffer) key.attachment();
+        socketChannel.write(buffer);
+        if (!buffer.hasRemaining()) {
+            log.info("Send message  : {}" ,socketChannel.getRemoteAddress());
+            socketChannel.register(selector, SelectionKey.OP_READ);
         }
-        buffer.flip();
-        String msg = StandardCharsets.UTF_8.decode(buffer).toString();
-        log.info("bytesRead : {}, msg : {}", bytesRead, msg);
+    }
+    private void recvMessage(Selector selector, SelectionKey key) throws IOException, InterruptedException {
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        log.info("SocketChannel client : {}", socketChannel);
+        buffer.clear();
+        int bytesRead = socketChannel.read(buffer);
+        if (bytesRead == -1) {                        //연결이 끊어졌을 때
+            log.info("Connection closed");
+          socketChannel.close();
+//          throw new InterruptedException();
+        }
+            buffer.flip();
+            //받은 메세지 찍어보기
+//            String msg = StandardCharsets.UTF_8.decode(buffer).toString();
+//            log.info("bytesRead : {}, msg : {}", bytesRead, msg);
+
+            //Echo 기능
+            byte[] bytes = new byte[buffer.limit()];
+            buffer.get(bytes);
+            System.out.println("Received message from " + socketChannel.getRemoteAddress() + ": " + new String(bytes));
+            socketChannel.register(selector, SelectionKey.OP_WRITE, ByteBuffer.wrap(bytes));
+
     }
 
-    private static void register(Selector selector, ServerSocketChannel serverSocket)
-            throws IOException {
-        SocketChannel client = serverSocket.accept();
-        if (client != null) {
-            client.configureBlocking(false);
-            client.register(selector, SelectionKey.OP_READ);
+    private void register(Selector selector, ServerSocketChannel serverSocket) throws IOException {
+        SocketChannel socketChannel = serverSocket.accept();
+        if (socketChannel != null) {
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_READ);
             System.out.println("connected new client.");
         }
     }
