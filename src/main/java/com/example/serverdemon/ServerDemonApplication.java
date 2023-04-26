60d4abaa5afb05dd6f1e02cd66b1d5d9fdeb0313
ziva456 <qkeksmsqkah@gmail.com>
2023-04-27 00:13:11 +0900

    연속된 Connect 적용

--- src/main/java/com/example/serverdemon/ServerDemonApplication.java
+++ src/main/java/com/example/serverdemon/ServerDemonApplication.java
@@ -3,10 +3,12 @@ package com.example.serverdemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ServerDemonApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(ServerDemonApplication.class, args);
         SocketChannelThread socketChannelThread = new SocketChannelThread();
         socketChannelThread.start();
--- src/main/java/com/example/serverdemon/SocketChannelThread.java
+++ src/main/java/com/example/serverdemon/SocketChannelThread.java
@@ -16,15 +16,19 @@ import java.util.Set;
 public class SocketChannelThread extends Thread{
     InetSocketAddress addr = new InetSocketAddress("localhost", 7777);
     private final ByteBuffer buffer = ByteBuffer.allocate(1024);
+    private final Selector selector;
+    private final ServerSocketChannel serverSocketChannel;
+    public SocketChannelThread() throws IOException{
+        selector = Selector.open();
+        serverSocketChannel  = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);				//서버소켓채널 비차단모드로 설정
+        serverSocketChannel.socket().bind(this.addr);				//서버주소 지정
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);	//연결요청 수락
+    }
+
     public void run() {
 
         try {
-            Selector selector = Selector.open();
-            ServerSocketChannel serverSocket  = ServerSocketChannel.open();
-            serverSocket.configureBlocking(false);				//소켓채널 비차단모드로 설정
-            serverSocket.socket().bind(this.addr);				//서버주소 지정
-            serverSocket.register(selector, SelectionKey.OP_ACCEPT);	//연결요청 수락모드로 설정
-
             while (!Thread.currentThread().isInterrupted()) {
 
                 if(selector.select()>0){
@@ -33,14 +37,17 @@ public class SocketChannelThread extends Thread{
                     while (it.hasNext()) {
                         SelectionKey key = it.next();
 
+
+//                        log.info("isValid : {}, isAcceptable() : {} ,isReadable() : {},  isWritable() : {}, isConnectable() : {}",key.isValid(), key.isAcceptable(), key.isReadable(), key.isWritable(), key.isConnectable());
+
+                        if (!key.isValid()) {
+                            log.info("Error !!!");
+                            continue;
+                        }
+
                         if (key.isAcceptable()) {
                             log.info("REGI");
-                            register(selector, serverSocket);
-                        }
-                        //Echo
-                        if (key.isWritable()) {
-                            log.info("SEND");
-                            sendMessage(selector,key);
+                            register(selector, serverSocketChannel);
                         }
 
                         if (key.isReadable()) {
@@ -48,11 +55,11 @@ public class SocketChannelThread extends Thread{
                             recvMessage(selector,key);
                         }
 
-                    it.remove();
+                        it.remove();
                     }
                 }
             }
-            serverSocket.close();
+            serverSocketChannel.close();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -60,15 +67,6 @@ public class SocketChannelThread extends Thread{
         }
     }
 
-    private void sendMessage(Selector selector, SelectionKey key) throws IOException, InterruptedException {
-        SocketChannel socketChannel = (SocketChannel) key.channel();
-        ByteBuffer buffer = (ByteBuffer) key.attachment();
-        socketChannel.write(buffer);
-        if (!buffer.hasRemaining()) {
-            log.info("Send message  : {}" ,socketChannel.getRemoteAddress());
-            socketChannel.register(selector, SelectionKey.OP_READ);
-        }
-    }
     private void recvMessage(Selector selector, SelectionKey key) throws IOException, InterruptedException {
         SocketChannel socketChannel = (SocketChannel) key.channel();
         log.info("SocketChannel client : {}", socketChannel);
@@ -76,26 +74,26 @@ public class SocketChannelThread extends Thread{
         int bytesRead = socketChannel.read(buffer);
         if (bytesRead == -1) {                        //연결이 끊어졌을 때
             log.info("Connection closed");
-          socketChannel.close();
+            socketChannel.close();
 //          throw new InterruptedException();
+            key.cancel();
         }
             buffer.flip();
-            //받은 메세지 찍어보기
+            //UTF_8 받은 메세지 찍어보기
 //            String msg = StandardCharsets.UTF_8.decode(buffer).toString();
 //            log.info("bytesRead : {}, msg : {}", bytesRead, msg);
 
             //Echo 기능
             byte[] bytes = new byte[buffer.limit()];
             buffer.get(bytes);
-            System.out.println("Received message from " + socketChannel.getRemoteAddress() + ": " + new String(bytes));
+            log.info("Received : {}, msg :{}", socketChannel.getRemoteAddress() , new String(bytes));
             socketChannel.register(selector, SelectionKey.OP_WRITE, ByteBuffer.wrap(bytes));
-
     }
 
     private void register(Selector selector, ServerSocketChannel serverSocket) throws IOException {
         SocketChannel socketChannel = serverSocket.accept();
         if (socketChannel != null) {
-            socketChannel.configureBlocking(false);
+            socketChannel.configureBlocking(false); //client 소켓채널 비차단모드로 설정
             socketChannel.register(selector, SelectionKey.OP_READ);
             System.out.println("connected new client.");
         }
